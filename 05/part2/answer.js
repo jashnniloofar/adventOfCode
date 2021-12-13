2dbe344085f78a10815fdbe6f3117df75e3a6fe2
Hojjat Jashnniloofar <hjash@imagineon.de>
2021-12-13 11:42:19 +0100

    Day 5

---
+++ 05/part2/answer.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt").toString();
+const lines = input.split(/\r?\n/);
+
+const vents = [];
+for (let index = 0; index < lines.length; index++) {
+    const tokens = lines[index].split(" -> ");
+    vents[index] = [];
+    vents[index].push(tokens[0].split(",").map((p) => parseInt(p)));
+    vents[index].push(tokens[1].split(",").map((p) => parseInt(p)));
+}
+
+const map = [];
+for (let i = 0; i < 1000; i++) {
+    map[i] = [];
+    for (let j = 0; j < 1000; j++) {
+        map[i].push(0);
+    }
+}
+
+function printMap(map) {
+    for (let index = 0; index < map.length; index++) {
+        console.log(`${map[index]}`);
+    }
+    console.log("------------------");
+}
+
+let result = 0;
+for (let index = 0; index < vents.length; index++) {
+    const vent = vents[index];
+    const directionX = vent[0][0] === vent[1][0] ? 0 : vent[0][0] > vent[1][0] ? -1 : 1;
+    const directionY = vent[0][1] === vent[1][1] ? 0 : vent[0][1] > vent[1][1] ? -1 : 1;
+    let x = vent[0][0];
+    let y = vent[0][1];
+    while (x !== vent[1][0] || y !== vent[1][1]) {
+        if (map[x][y] === 1) result++;
+        map[x][y]++;
+        x += directionX;
+        y += directionY;
+    }
+    if (map[x][y] === 1) result++;
+    map[x][y]++;
+}
+
+console.log(`Answer: ${result}`);
