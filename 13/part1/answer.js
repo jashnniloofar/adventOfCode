c6d9a93a7a2f1e3ff40522a0e72ef8da781af925
Hojjat Jashnniloofar <hjash@imagineon.de>
2021-12-14 10:41:09 +0100

    Day 13

---
+++ 13/part1/answer.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt").toString();
+const lines = input.split(/\r?\n/);
+const dots = new Map();
+const folds = [];
+
+lines.forEach((line) => {
+    if (line.startsWith("fold along")) {
+        const fold = { direction: "x", position: parseInt(line.split("=")[1]) };
+        if (line.startsWith("fold along y")) {
+            fold.direction = "y";
+        }
+        folds.push(fold);
+    } else {
+        if (line.length > 0) {
+            dots.set(line, 1);
+        }
+    }
+});
+
+function foldPaper(dots, fold) {
+    dots.forEach((val, key) => {
+        const dot = { x: parseInt(key.split(",")[0]), y: parseInt(key.split(",")[1]) };
+        if (fold.direction === "x" && dot.x > fold.position && dot.x <= fold.position * 2) {
+            dots.delete(key);
+            dots.set(`${fold.position * 2 - dot.x},${dot.y}`, 1);
+        }
+        if (fold.direction === "y" && dot.y > fold.position && dot.y <= fold.position * 2) {
+            dots.delete(key);
+            dots.set(`${dot.x},${fold.position * 2 - dot.y}`, 1);
+        }
+    });
+}
+
+foldPaper(dots, folds[0]);
+
+console.log(`Answer: ${dots.size}`);
