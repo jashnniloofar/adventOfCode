ca666ff0c39e43a53ce30cb85e7e5306ae255525
Hojjat Jashnniloofar <hjash@imagineon.de>
2021-12-26 14:52:37 +0100

    Day 4 - 2015

---
+++ 2015/04/part1/answer.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const crypto = require("crypto");
+let start = Date.now();
+const input = fs.readFileSync("./input.txt").toString();
+
+function md5(input) {
+    let md5sum = crypto.createHash("md5");
+    md5sum.update(input);
+    return md5sum.digest().toString("hex");
+}
+let mined = false;
+let nonce = 0;
+while (!mined) {
+    nonce++;
+    mined = md5(input + nonce).startsWith("00000");
+}
+
+console.log(`Answer: ${nonce}`);
+console.log(`Run time: ${Date.now() - start} ms`);
