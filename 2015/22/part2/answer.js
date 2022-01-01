ef71da9c440f194a0408486fa317378f862314c6
Hojjat Jashnniloofar <hjash@imagineon.de>
2022-01-01 13:45:31 +0100

    Day 22 - 2015

---
+++ 2015/22/part2/answer.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+let start = Date.now();
+const input = fs.readFileSync("./input.txt").toString();
+const lines = input.split(/\r?\n/);
+
+let minMana = Number.MAX_SAFE_INTEGER;
+function nextTurn(playerHp, bossHp, mana, usedMana, shield, poison, recharge) {
+    // console.log(playerHp, bossHp, mana, usedMana, shield, poison, recharge);
+    if (mana < 0) return;
+    if (usedMana >= minMana) return;
+    //Boss attack
+    playerHp = shield > 0 ? playerHp - 1 : playerHp - 8;
+    //Boss killed?
+    if (bossHp <= 0) return (minMana = usedMana);
+    //Player Killed?
+    if (playerHp <= 1) return;
+    if (poison > 0) bossHp = bossHp - Math.min(poison, 2) * 3;
+    if (recharge > 0) mana = mana + Math.min(recharge, 2) * 101;
+
+    poison = Math.max(0, poison - 2);
+    shield = Math.max(0, shield - 2);
+    recharge = Math.max(0, recharge - 2);
+    //Magic Missile
+    nextTurn(playerHp - 1, bossHp - 4, mana - 53, usedMana + 53, shield, poison, recharge);
+    //Drain
+    nextTurn(playerHp + 1, bossHp - 2, mana - 73, usedMana + 73, shield, poison, recharge);
+    if (shield === 0) nextTurn(playerHp - 1, bossHp, mana - 113, usedMana + 113, 6, poison, recharge);
+    //Poison
+    if (poison === 0) nextTurn(playerHp - 1, bossHp - 3, mana - 173, usedMana + 173, shield, 5, recharge);
+    //Recharge
+    if (recharge === 0) nextTurn(playerHp - 1, bossHp, mana - 229, usedMana + 229, shield, poison, 5);
+}
+nextTurn(58, 55, 500, 0, 0, 0, 0);
+
+console.log(`Answer: ${minMana}`);
+console.log(`Run time: ${Date.now() - start} ms`);
