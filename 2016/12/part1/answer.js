8ec938eac629013613e7b2dc7a4caa1b25e9a3cc
Hojjat Jashnniloofar <hjash@imagineon.de>
2022-01-04 11:14:21 +0100

    Day 12 - 2016

---
+++ 2016/12/part1/answer.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let start = Date.now();
+const input = fs.readFileSync("./input.txt").toString();
+const instructions = input.split(/\r?\n/).map((ins) => ins.split(" "));
+const state = { a: 0, b: 0, c: 0, d: 0 };
+let index = 0;
+while (index < instructions.length) {
+    const [command, ...operands] = instructions[index];
+    if (command === "cpy") {
+        if (["a", "b", "c", "d"].includes(operands[0])) state[operands[1]] = state[operands[0]];
+        else state[operands[1]] = parseInt(operands[0]);
+    }
+    if (command === "inc") state[operands[0]]++;
+    if (command === "dec") state[operands[0]]--;
+    if (command === "jnz" && state[operands[0]] !== 0) index += parseInt(operands[1]);
+    else index++;
+}
+
+console.log(`Answer: ${state.a}`);
+console.log(`Run time: ${Date.now() - start} ms`);
---
+++ 2016/12/part2/question.txt
@@ -0,0 +1,4 @@
+--- Part Two ---
+As you head down the fire escape to the monorail, you notice it didn't start; register c needs to be initialized to the position of the ignition key.
+
+If you instead initialize register c to be 1, what value is now left in register a?
