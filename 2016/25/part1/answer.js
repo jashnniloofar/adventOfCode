cb63f96d0e41bc1377f2147fa55f2ace67b2b5df
Hojjat Jashnniloofar <hjash@imagineon.de>
2022-01-06 15:27:15 +0100

    Day 25 - 2016

---
+++ 2016/25/part1/answer.js
@@ -0,0 +1,4 @@
+let start = Date.now();
+// machine prints 4 * 633 + a in binary format
+console.log(`Answer: ${parseInt("101010101010", 2) - 4 * 633}`);
+console.log(`Run time: ${Date.now() - start} ms`);
