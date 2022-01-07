c6a57ed18db49b5efa5db33aefec9081acfd6c89
Hojjat Jashnniloofar <hjash@imagineon.de>
2022-01-07 13:50:53 +0100

    Day 06 - 2017

---
+++ 2017/06/part2/answer.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let start = Date.now();
+const input = fs.readFileSync("./input.txt").toString();
+const blocks = input.split("\t").map((s) => parseInt(s));
+const map = new Map();
+let step = 0;
+while (!map.has(blocks.join(","))) {
+    map.set(blocks.join(","), step);
+    const max = Math.max(...blocks);
+    const index = blocks.findIndex((block) => block === max);
+    blocks[index] = 0;
+    for (let i = 1; i <= max; i++) {
+        blocks[(index + i) % blocks.length]++;
+    }
+    step++;
+}
+
+console.log(`Answer: ${step - map.get(blocks.join(","))}`);
+console.log(`Run time: ${Date.now() - start} ms`);
