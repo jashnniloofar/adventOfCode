f31a00b92cd08fa06fff778457fff1107e208c7b
Hojjat Jashnniloofar <hjash@imagineon.de>
2022-01-25 21:31:03 +0100

    2019 - Day 04

---
+++ 2019/04/part1/answer.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let start = Date.now();
+const input = fs.readFileSync("./input.txt").toString();
+const [first, last] = input.split("-").map(Number);
+
+let count = 0;
+for (let n = first; n < last; n++) {
+    const s = n.toString();
+    if (/(\d)\1/.test(s)) {
+        let enc = true;
+        for (let j = 0; j < s.length - 1; j++) {
+            if (s[j + 1] < s[j]) enc = false;
+        }
+        count += enc;
+    }
+}
+
+console.log(`Answer: ${count}`);
+console.log(`Run time: ${Date.now() - start} ms`);
