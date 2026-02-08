cd62099cbd9b6a7a270422631986fa9254a9e230
Hojjat Jashnniloofar <jashnniloofar@gmail.com>
2026-02-08 18:14:42 +0100

    format code

--- 2025/04/answer.py
+++ 2025/04/answer.py
@@ -3,13 +3,14 @@ with open("./input.txt", "r") as f:
     lines = [line.strip() for line in f]
 lines = [list(line) for line in lines]
 
+
 def is_paper(input_map, row, col):
     if row < 0 or row >= len(input_map) or col < 0 or col >= len(input_map[0]):
         return False
     return input_map[row][col] == "@"
 
 
-def part1(input_map, remove_papers= False):
+def part1(input_map, remove_papers=False):
     sum_accesible = 0
     accessible_papers = []
     for row in range(len(input_map)):
@@ -19,10 +20,11 @@ def part1(input_map, remove_papers= False):
                 for i in range(-1, 2):
                     for j in range(-1, 2):
                         if i != 0 or j != 0:
-                            adjacent_paper += is_paper(input_map, row + i, col + j)
+                            adjacent_paper += is_paper(input_map,
+                                                       row + i, col + j)
                 if adjacent_paper < 4:
                     sum_accesible += 1
-                    accessible_papers.append([row,col])
+                    accessible_papers.append([row, col])
     if remove_papers:
         for accessible_paper in accessible_papers:
             input_map[accessible_paper[0]][accessible_paper[1]] = '.'
